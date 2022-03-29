40f460b57f1195ab18f514fc43c6feb0f53a45d2
Santiago Mozo <smozo615@gmail.com>
2022-03-29 01:10:37 -0500

    alias, logos, icons

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,15 @@
 import React from "react";
-import "../styles/Header.scss";
+import ICON_MENU from '@icons/icon_menu.svg';
+import LOGO_YARD_SALE from '@logos/logo_yard_sale.svg';
+import ICON_SHOPPING_CART from '@icons/icon_shopping_cart.svg';
+import "@styles/Header.scss";
 
 const Header = () => {
   return (
     <nav>
-      <img src="./icons/icon_menu.svg" alt="menu" className="menu" />
+      <img src={ICON_MENU} alt="menu" className="menu" />
       <div className="navbar-left">
-        <img src="./logos/logo_yard_sale.svg" alt="logo" className="logo" />
+        <img src={LOGO_YARD_SALE} alt="logo" className="nav-logo" />
         <ul>
           <li>
             <a href="/">All</a>
@@ -32,7 +35,7 @@ const Header = () => {
         <ul>
           <li className="navbar-email">platzi@example.com</li>
           <li className="navbar-shopping-cart">
-            <img src="./icons/icon_shopping_cart.svg" alt="shopping cart" />
+            <img src={ICON_SHOPPING_CART} alt="shopping cart" />
             <div>2</div>
           </li>
         </ul>
