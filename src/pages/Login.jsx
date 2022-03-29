312b8d257092965b5dbfb186be2b60bff7f36649
Santiago Mozo <smozo615@gmail.com>
2022-03-29 16:18:15 -0500

    hook: useRef

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,19 +1,31 @@
-import React from "react";
-import logoYardSale from '@logos/logo_yard_sale.svg';
+import React, { useRef } from "react";
+import logoYardSale from "@logos/logo_yard_sale.svg";
 import "../styles/Login.scss";
 
 const Login = () => {
+  const form = useRef(null);
+
+  const haddleSubmit = () => {
+    event.preventDefault();
+    const formData = new FormData(form.current);
+    const data = {
+      username: formData.get("email"),
+      password: formData.get("password"),
+    };
+    console.log(data);
+  };
+
   return (
     <div className="Login">
       <div className="Login-container">
         <img src={logoYardSale} alt="logo" className="logo" />
-        <form action="/" className="form">
+        <form action="/" className="form" ref={form}>
           <label htmlFor="email" className="label">
             Email address
           </label>
           <input
             type="text"
-            id="email"
+            name="email"
             placeholder="platzi@example.cm"
             className="input input-email"
           />
@@ -22,15 +34,16 @@ const Login = () => {
           </label>
           <input
             type="password"
-            id="password"
+            name="password"
             placeholder="*********"
             className="input input-password"
           />
-          <input
-            type="submit"
-            value="Log in"
+          <button
             className="primary-button login-button"
-          />
+            onClick={haddleSubmit}
+          >
+            Log in
+          </button>
           <a href="/">Forgot my password</a>
         </form>
         <button className="secondary-button signup-button">Sign up</button>
